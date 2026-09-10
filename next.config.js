2b8cb2a97ac2ca12a8aa8fa4b9b3e0b3604c3966
henri08520852 <henri.steckhan@altoris.one>
2026-09-10 09:54:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['cheerio'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
